a6ffc89d0ceaf8233c41a361cc41056b9991fb37
Ajeet T S <ajeet.ts@citi.com>
2024-10-30 22:17:08 +0530

    sendfile with filename

--- src/api/v2/index.ts
+++ src/api/v2/index.ts
@@ -1,9 +1,27 @@
 import { Request, Response, Router } from "express";
+import path from "path";
+import fs from "fs";
 
 const v2Router = Router();
 
-v2Router.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ message: "v2 api" });
+v2Router.get("/:name", (req: Request, res: Response) => {
+  try {
+    const pathToFile = path.join(
+      __dirname,
+      "../../../",
+      "_apps",
+      req.params.name
+    );
+    res.header("Content-Type", "application/json");
+
+    if (!fs.existsSync(pathToFile)) throw new Error("No such file found");
+
+    return res.sendFile(pathToFile);
+  } catch (error) {
+    // res.header("Content-Type", "application/json");
+    // return res.json({ message: "not found" });
+    return res.sendFile("");
+  }
 });
 
 export default v2Router;
